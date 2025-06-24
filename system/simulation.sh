c021488373663b430533350fc65633b4cb1b2fc9
Renan Morais <renan.smorais2001@gmail.com>
2025-06-24 12:53:21 -0300

    new simulation

--- system/simulation.sh
+++ system/simulation.sh
@@ -4,24 +4,10 @@ python generate_Cifar10.py noniid False dir 0.1
 python generate_MNIST.py noniid False dir 0.1
 cd ../system
 
-## MNIST
-python main.py -nc 100 -gr 30 --dataset_concept_drift='MNIST' --new_alpha=1 --initial_alpha=0.1 --rounds_concept_drift=15 -go concept_drift_mnist_0.1_1_rebalance_loss -pe loss -re
-python main.py -nc 100 -gr 30 --dataset_concept_drift='MNIST' --new_alpha=1 --initial_alpha=0.1 --rounds_concept_drift=15 -go concept_drift_mnist_0.1_1_rebalance_acc -pe acc -re
-python main.py -nc 100 -gr 30 --dataset_concept_drift='MNIST' --new_alpha=1 --initial_alpha=0.1 --rounds_concept_drift=15 -go concept_drift_mnist_0.1_1_rebalance_param -pe parameters -re
-
-python main.py -nc 100 -gr 30 --dataset_concept_drift='MNIST' --new_alpha=0.1 --initial_alpha=1 --rounds_concept_drift=15 -go concept_drift_mnist_1_0.1_rebalance_loss -pe loss -re
-python main.py -nc 100 -gr 30 --dataset_concept_drift='MNIST' --new_alpha=0.1 --initial_alpha=1 --rounds_concept_drift=15 -go concept_drift_mnist_1_0.1_rebalance_acc -pe acc -re
-python main.py -nc 100 -gr 30 --dataset_concept_drift='MNIST' --new_alpha=0.1 --initial_alpha=1 --rounds_concept_drift=15 -go concept_drift_mnist_1_0.1_rebalance_param -pe parameters -re
-
-
 ## Cifar10
-python main.py -nc 100 -gr 100 --dataset_concept_drift='Cifar10' --new_alpha=1 --initial_alpha=0.1 --rounds_concept_drift=50 -go concept_drift_cifar10_0.1_1_loss -pe loss -re
-python main.py -nc 100 -gr 100 --dataset_concept_drift='Cifar10' --new_alpha=1 --initial_alpha=0.1 --rounds_concept_drift=50 -go concept_drift_cifar10_0.1_1_acc -pe acc -re
-python main.py -nc 100 -gr 100 --dataset_concept_drift='Cifar10' --new_alpha=1 --initial_alpha=0.1 --rounds_concept_drift=50 -go concept_drift_cifar10_0.1_1_param -pe parameters -re
-
-python main.py -nc 100 -gr 100 --dataset_concept_drift='Cifar10' --new_alpha=0.1 --initial_alpha=1 --rounds_concept_drift=50 -go concept_drift_cifar10_1_0.1_loss -pe loss -re
-python main.py -nc 100 -gr 100 --dataset_concept_drift='Cifar10' --new_alpha=0.1 --initial_alpha=1 --rounds_concept_drift=50 -go concept_drift_cifar10_1_0.1_acc -pe acc -re
-python main.py -nc 100 -gr 100 --dataset_concept_drift='Cifar10' --new_alpha=0.1 --initial_alpha=1 --rounds_concept_drift=50 -go concept_drift_cifar10_1_0.1_param -pe parameters -re
+python main.py -nc 100 -gr 100 --dataset_concept_drift='Cifar10' -t 5 --new_alpha=1 --initial_alpha=0.1 --rounds_concept_drift=50 -go concept_drift_cifar10_0.1_1_loss -pe loss -re
+python main.py -nc 100 -gr 100 --dataset_concept_drift='Cifar10' -t 5 --new_alpha=1 --initial_alpha=0.1 --rounds_concept_drift=50 -go concept_drift_cifar10_0.1_1_acc -pe acc -re
+python main.py -nc 100 -gr 100 --dataset_concept_drift='Cifar10' -t 5 --new_alpha=1 --initial_alpha=0.1 --rounds_concept_drift=50 -go concept_drift_cifar10_0.1_1_param -pe parameters -re
 
 ######################################################################
 
@@ -32,10 +18,11 @@ python generate_MNIST.py noniid False dir 0.1
 cd ../system
 
 ## MNIST
-python main.py -nc 100 -gr 30 --dataset_label_shift='MNIST' --rounds_label_shift=15 -go data_shift_mnist_rebalance_acc   -pe acc        -re
-python main.py -nc 100 -gr 30 --dataset_label_shift='MNIST' --rounds_label_shift=15 -go data_shift_mnist_rebalance_loss  -pe loss       -re
-python main.py -nc 100 -gr 30 --dataset_label_shift='MNIST' --rounds_label_shift=15 -go data_shift_mnist_rebalance_param -pe parameters -re
+python main.py -nc 100 -gr 30 --dataset_label_shift='MNIST' -t 5 --rounds_label_shift=15 -go data_shift_mnist_rebalance_acc   -pe acc        -re
+python main.py -nc 100 -gr 30 --dataset_label_shift='MNIST' -t 5 --rounds_label_shift=15 -go data_shift_mnist_rebalance_loss  -pe loss       -re
+python main.py -nc 100 -gr 30 --dataset_label_shift='MNIST' -t 5 --rounds_label_shift=15 -go data_shift_mnist_rebalance_param -pe parameters -re
+
 ## Cifar10
-python main.py -nc 100 -gr 100 --dataset_label_shift='Cifar10' --rounds_label_shift=50 -go data_shift_cifar10_rebalance_acc   -pe acc        -re
-python main.py -nc 100 -gr 100 --dataset_label_shift='Cifar10' --rounds_label_shift=50 -go data_shift_cifar10_rebalance_loss  -pe loss       -re
-python main.py -nc 100 -gr 100 --dataset_label_shift='Cifar10' --rounds_label_shift=50 -go data_shift_cifar10_rebalance_param -pe parameters -re
+python main.py -nc 100 -gr 100 --dataset_label_shift='Cifar10' -t 5 --rounds_label_shift=50 -go data_shift_cifar10_rebalance_acc   -pe acc        -re
+python main.py -nc 100 -gr 100 --dataset_label_shift='Cifar10' -t 5 --rounds_label_shift=50 -go data_shift_cifar10_rebalance_loss  -pe loss       -re
+python main.py -nc 100 -gr 100 --dataset_label_shift='Cifar10' -t 5 --rounds_label_shift=50 -go data_shift_cifar10_rebalance_param -pe parameters -re
--- system/simulation1.sh
+++ system/simulation1.sh
@@ -4,16 +4,11 @@ python generate_Cifar10.py noniid False dir 0.1
 python generate_MNIST.py noniid False dir 0.1
 cd ../system
 
-## MNIST
-python main.py -nc 100 -gr 30 --dataset_concept_drift='MNIST' --new_alpha=0.1 --initial_alpha=1 --rounds_concept_drift=15 -go concept_drift_mnist_1_0.1
-python main.py -nc 100 -gr 30 --dataset_concept_drift='MNIST' --new_alpha=1 --initial_alpha=0.1 --rounds_concept_drift=15 -go concept_drift_mnist_0.1_1
-python main.py -nc 100 -gr 30 --dataset_concept_drift='MNIST' --new_alpha=1 --initial_alpha=0.1 --rounds_concept_drift=15 -go concept_drift_mnist_0.1_1_rebalance -ma
-
 ## Cifar10
-python main.py -nc 100 -gr 100 --dataset_concept_drift='Cifar10' --new_alpha=0.1 --initial_alpha=1 --rounds_concept_drift=50 -go concept_drift_cifar10_1_0.1
-python main.py -nc 100 -gr 100 --dataset_concept_drift='Cifar10' --new_alpha=0.1 --initial_alpha=1 --rounds_concept_drift=50 -go concept_drift_cifar10_1_0.1_rebalance -ma
-python main.py -nc 100 -gr 100 --dataset_concept_drift='Cifar10' --new_alpha=1 --initial_alpha=0.1 --rounds_concept_drift=50 -go concept_drift_cifar10_0.1_1
-python main.py -nc 100 -gr 100 --dataset_concept_drift='Cifar10' --new_alpha=1 --initial_alpha=0.1 --rounds_concept_drift=50 -go concept_drift_cifar10_0.1_1_rebalance -ma
+python main.py -nc 100 -gr 100 --dataset_concept_drift='Cifar10' -t 5 --new_alpha=0.1 --initial_alpha=1 --rounds_concept_drift=50 -go concept_drift_cifar10_1_0.1
+python main.py -nc 100 -gr 100 --dataset_concept_drift='Cifar10' -t 5 --new_alpha=0.1 --initial_alpha=1 --rounds_concept_drift=50 -go concept_drift_cifar10_1_0.1_rebalance -ma
+python main.py -nc 100 -gr 100 --dataset_concept_drift='Cifar10' -t 5 --new_alpha=1 --initial_alpha=0.1 --rounds_concept_drift=50 -go concept_drift_cifar10_0.1_1
+python main.py -nc 100 -gr 100 --dataset_concept_drift='Cifar10' -t 5 --new_alpha=1 --initial_alpha=0.1 --rounds_concept_drift=50 -go concept_drift_cifar10_0.1_1_rebalance -ma
 
 ######################################################################
 
@@ -24,8 +19,8 @@ python generate_MNIST.py noniid False dir 0.1
 cd ../system
 
 ## MNIST
-python main.py -nc 100 -gr 30 --dataset_label_shift='MNIST' --rounds_label_shift=15 -go data_shift_mnist
-python main.py -nc 100 -gr 30 --dataset_label_shift='MNIST' --rounds_label_shift=15 -go data_shift_mnist_rebalance -ma
+python main.py -nc 100 -gr 30 --dataset_label_shift='MNIST' -t 5 --rounds_label_shift=15 -go data_shift_mnist
+python main.py -nc 100 -gr 30 --dataset_label_shift='MNIST' -t 5 --rounds_label_shift=15 -go data_shift_mnist_rebalance -ma
 ## Cifar10
-python main.py -nc 100 -gr 100 --dataset_label_shift='Cifar10' --rounds_label_shift=50 -go data_shift_cifar10
-python main.py -nc 100 -gr 100 --dataset_label_shift='Cifar10' --rounds_label_shift=50 -go data_shift_cifar10_rebalance -ma
+python main.py -nc 100 -gr 100 --dataset_label_shift='Cifar10' -t 5 --rounds_label_shift=50 -go data_shift_cifar10
+python main.py -nc 100 -gr 100 --dataset_label_shift='Cifar10' -t 5 --rounds_label_shift=50 -go data_shift_cifar10_rebalance -ma
